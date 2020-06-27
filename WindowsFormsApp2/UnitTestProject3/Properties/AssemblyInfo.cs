5cd521407536fde24ea3e06fbc1ecaa716b3aed9
Maciej Zalewski <w60127@student.wsiz.rzeszow.pl>
2020-06-27 19:40:57 +0200

    added folder of second test

---
+++ WindowsFormsApp2/UnitTestProject3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject3")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fce1cd7d-c6bf-48fd-aeb9-a9b0220a09a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WindowsFormsApp2/UnitTestProject3/sprawdzenie polskości.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApp2;
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void testPolskosci()
+        {   //arrange
+            string testowaetykieta = "grzegorz brzęszczyszczykiewicz";
+
+            //act
+
+            bool test1 = new Form1().czyZawieraPolskieZnaki(testowaetykieta);
+
+            //assert
+            Assert.IsTrue(test1);
+
+        }
+    }
+}
